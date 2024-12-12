87425a1b4782abed93bb702a3e236f08d815a694
angiyy <167371013+angiyy@users.noreply.github.com>
2024-12-12 00:15:48 -0600

    Update settings.py
    cambio eliminar mas debugs

--- hotel_reservations/settings.py
+++ hotel_reservations/settings.py
@@ -26,8 +26,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'True'
-DEBUG = 'RENDER' not in os.environ
-DEBUG=os.getenv('DEBUG', 'False') == 'True'
 
 # https://docs.djangoproject.com/en/3.0/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ['*.onrender.com']
