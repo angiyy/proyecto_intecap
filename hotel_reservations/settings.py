d1946703dde99d61c9565ed09315bc7d854a42c0
J0sedavid1 <yaxangela89@gmail.com>
2024-12-11 21:11:04 -0600

    Actualizacion error de escritura

--- hotel_reservations/settings.py
+++ hotel_reservations/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middlware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'hotel_reservations.urls'
